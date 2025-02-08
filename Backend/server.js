40f3dd3b0caf6a9b45a2a0f9b1a6518bd63a27dc
AhmadJaber12 <zalatahmad87@gmail.com>
2025-02-08 22:51:21 +0200

    Final_Touch

--- Backend/server.js
+++ Backend/server.js
@@ -16,9 +16,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(productRouter);
-    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.use(express.static(path.join(__dirname,"/Frontend/dist")))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+        res.sendFile(path.resolve(__dirname,"Frontend","dist","index.html"))
     })
 
 app.listen(PORT,()=>
